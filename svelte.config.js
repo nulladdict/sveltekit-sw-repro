17e6807ac2d0edebc8d1a3d68b01c8d0fed1fc17
nulladdict <nulladdicted@gmail.com>
2022-01-20 12:51:49 +0500

    feat: add repro case

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +6,11 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+		// this are the culprits, if both are enabled service worker registration code is not injected
+		hydrate: false,
+		router: false,
 	}
 };
 
